ad0b0729376cc67c32f8b011c50144d7083d3917
Axel <cymbalaxel@gmail.com>
2022-07-29 18:30:15 -0300

    se agrego una funcion calculateWinner y se modifico el componente Board

--- src/index.js
+++ src/index.js
@@ -56,7 +56,13 @@ Componente SQUARE en componente clase:
     }
   
     render() {
-      const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      let status;
+      const winner = calculateWinner(this.state.squares);
+      if(winner){
+        status = `Winner: ${winner}`;
+      } else{
+        status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`
+      }
   
       return (
         <div>
@@ -95,6 +101,28 @@ Componente SQUARE en componente clase:
         </div>
       );
     }
+
+
+  }
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for(let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
